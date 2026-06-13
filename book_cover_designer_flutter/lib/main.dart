bb28674fbaca0d03ff39672de051eb391138ca99
Kenneth Brewer <kennethbrewer3@gmail.com>
2026-06-13 03:44:48 -0400

    Added theming to the app

--- book_cover_designer_flutter/lib/main.dart
+++ book_cover_designer_flutter/lib/main.dart
@@ -1,33 +1,44 @@
 import 'package:book_cover_designer_flutter/app/app.locator.dart';
 import 'package:book_cover_designer_flutter/app/app.router.dart';
+import 'package:book_cover_designer_flutter/services/theme_service.dart';
+import 'package:book_cover_designer_flutter/ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:stacked_services/stacked_services.dart';
+
+late final ThemeService themeService;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await setupLocator();
+  themeService = await ThemeService.create();
 
-  runApp(const EbookCoverGeneratorApp());
+  runApp(EbookCoverGeneratorApp(themeService: themeService));
 }
 
 class EbookCoverGeneratorApp extends StatelessWidget {
-  const EbookCoverGeneratorApp({super.key});
+  const EbookCoverGeneratorApp({super.key, required this.themeService});
 
+  final ThemeService themeService;
   static final _router = StackedRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Book Cover Designer',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      localizationsDelegates: const [
-        FlutterQuillLocalizations.delegate,
-      ],
-      navigatorKey: StackedService.navigatorKey,
-      onGenerateRoute: _router.onGenerateRoute,
-      initialRoute: Routes.homeView,
+    return ValueListenableBuilder<AppThemeVariant>(
+      valueListenable: themeService.variant,
+      builder: (context, variant, _) {
+        return MaterialApp(
+          title: 'Book Cover Designer',
+          theme: AppTheme.forVariant(variant),
+          localizationsDelegates: const [
+            FlutterQuillLocalizations.delegate,
+          ],
+          navigatorKey: StackedService.navigatorKey,
+          onGenerateRoute: _router.onGenerateRoute,
+          initialRoute: Routes.homeView,
+        );
+      },
     );
   }
 }
